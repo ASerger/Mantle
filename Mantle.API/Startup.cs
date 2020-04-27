bca9be125e6ba617a6f57a760c07b132e1167c3c
serger <aserger1@gmail.com>
2020-04-27 14:44:58 -0400

    add repo project and scaffold DI

--- Mantle.API/Startup.cs
+++ Mantle.API/Startup.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Mantle.Repository.Database;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +28,8 @@ namespace Mantle.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddLogging();
+            services.AddDbContext<MantleDbContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:MantleDb"]));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ Mantle.Repository/Database/MantleDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mantle.Repository.Database
+{
+    public class MantleDbContext : DbContext
+    {
+        public MantleDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+    }
+}
