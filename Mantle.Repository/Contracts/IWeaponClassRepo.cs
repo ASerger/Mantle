2f5728b4887fcede6f96d09613fe0d722da3cbfe
Dan <drasmussen49@gmail.com>
2020-05-22 13:30:57 -0400

    Created interface IWeaponClassRepo and repo WeaponClassRepository for new data model and set up their inheritance

---
+++ Mantle.Repository/Contracts/IWeaponClassRepo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Mantle.DataModels.Models;
+
+namespace Mantle.Repository.Contracts
+{
+    public interface IWeaponClassRepo<T> : IBaseRepository<WeaponClass>
+    {
+    }
+}
---
+++ Mantle.Repository/DataRepo/WeaponClassRepository.cs
@@ -0,0 +1,16 @@
+﻿using Mantle.DataModels.Models;
+using Mantle.Repository.Contracts;
+using Mantle.Repository.Database;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mantle.Repository.DataRepo
+{
+    public class WeaponClassRepository<T> : BaseRepository<WeaponClass>, IWeaponClassRepo<WeaponClass>
+    {
+        public WeaponClassRepository(MantleDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
