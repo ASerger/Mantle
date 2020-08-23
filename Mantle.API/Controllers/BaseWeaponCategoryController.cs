155589430216a01e64197501a38a0e737a01a6df
serger <aserger1@gmail.com>
2020-08-23 13:46:05 -0400

    connected controller to loot layer

--- Mantle.API/Controllers/BaseWeaponCategoryController.cs
+++ Mantle.API/Controllers/BaseWeaponCategoryController.cs
@@ -2,14 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Mantle.DataModels.Models;
+using Mantle.Loot.Contracts;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace Mantle.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Produces("application/json")]
+    [Authorize]
     public class BaseWeaponCategoryController : ControllerBase
     {
+        private IBaseWeaponCategoryLoot _baseWeaponCategoryLoot;
+        private readonly ILogger<BaseWeaponCategoryController> _logger;
+
+        public BaseWeaponCategoryController(IBaseWeaponCategoryLoot baseWeaponCategoryLoot,
+            ILogger<BaseWeaponCategoryController> logger)
+        {
+            _baseWeaponCategoryLoot = baseWeaponCategoryLoot;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type=typeof(List<BaseWeaponCategory>))]
+        [AllowAnonymous]
+        public async Task<IEnumerable<BaseWeaponCategory>> Get()
+        {
+            return await _baseWeaponCategoryLoot.GetAllAsync();
+        }
     }
 }
