d0c20cfd08da22de40dea07229c45607ece15ede
serger <aserger1@gmail.com>
2020-09-03 22:06:30 -0400

    name modification and NYI exception added.

---
+++ Mantle.Loot/Contracts/Mappers/IBaseMapper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mantle.Loot.Contracts.Mappers
+{
+    public interface IBaseMapper<T, D> where T: class where D: class
+    {
+        Task<D> MapDataToDomainAsync(T dataModel);
+        Task<IEnumerable<D>> MapDataToDomainAsync(IEnumerable<T> dataModel);
+        Task<T> MapDomainToDataAsync(D domainModel);
+        Task<IEnumerable<T>> MapDomainToDataAsync(IEnumerable<D> domainModel);
+    }
+}
---
+++ Mantle.Loot/Mappers/BaseWeaponCategoryMapper.cs
@@ -0,0 +1,33 @@
+﻿using Data = Mantle.DataModels.Models;
+using Domain = Mantle.DomainModels.Models;
+using Mantle.Loot.Contracts.Mappers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mantle.Loot.Mappers
+{
+    public class BaseWeaponCategoryMapper : IBaseMapper<Data.BaseWeaponCategory, Domain.BaseWeaponCategory>
+    {
+        Task<Domain.BaseWeaponCategory> IBaseMapper<Data.BaseWeaponCategory, Domain.BaseWeaponCategory>.MapDataToDomainAsync(Data.BaseWeaponCategory dataModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<IEnumerable<Domain.BaseWeaponCategory>> IBaseMapper<Data.BaseWeaponCategory, Domain.BaseWeaponCategory>.MapDataToDomainAsync(IEnumerable<Data.BaseWeaponCategory> dataModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<Data.BaseWeaponCategory> IBaseMapper<Data.BaseWeaponCategory, Domain.BaseWeaponCategory>.MapDomainToDataAsync(Domain.BaseWeaponCategory domainModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<IEnumerable<Data.BaseWeaponCategory>> IBaseMapper<Data.BaseWeaponCategory, Domain.BaseWeaponCategory>.MapDomainToDataAsync(IEnumerable<Domain.BaseWeaponCategory> domainModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
