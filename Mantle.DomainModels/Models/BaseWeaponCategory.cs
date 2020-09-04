977eaecc9a9dfd3ccc67e8bcd330b7a394d0d440
serger <aserger1@gmail.com>
2020-09-03 21:01:18 -0400

    started implementation of a mapping layer

---
+++ Mantle.DomainModels/Models/BaseWeaponCategory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Mantle.DomainModels.Models
+{
+    public class BaseWeaponCategory
+    {
+        public int Id { get; set; }
+        public DateTimeOffset ModifiedOn { get; set; }
+        public string ModifiedBy { get; set; }
+
+        public string WeaponCategory { get; set; }
+        public double Cost { get; set; }
+        public double Weight { get; set; }
+        public bool IsMartial { get; set; }
+        public bool IsRange { get; set; }
+
+        public int BaseDamageTypeId { get; set; }
+        public int BaseDiceId { get; set; }
+
+        public string BaseDamageType { get; set; }
+        public string BaseDice { get; set; }
+    }
+}
---
+++ Mantle.Loot/Contracts/Mappers/IBaseWeaponCategoryMapper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mantle.Loot.Contracts.Mappers
+{
+    public interface IBaseWeaponCategoryMapper<T, D> where T: class where D: class
+    {
+        Task<D> MapDataToDomain(T dataModel);
+        Task<T> MapDomainToData(D domainModel);
+    }
+}
